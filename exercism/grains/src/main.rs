7988b0362323db5e80202d7f379f57326b11e3a1
Ludorg (WSL at Pyremius) <ludorg@ludorg.net>
2023-12-31 01:56:18 +0100

    exercism / grains

---
+++ exercism/grains/src/main.rs
@@ -0,0 +1,81 @@
+mod grains {
+    pub fn square(s: u32) -> u64 {
+        if s == 0 || s > 64 {
+            panic!("Square must be between 1 and 64")
+        }
+        (2_u64).pow(s - 1)
+    }
+
+    pub fn total() -> u64 {
+        // let mut ret = 0;
+        // for s in 1..=64 {
+        //     ret += square(s)
+        // }
+        // ret
+        (1..=64).map(square).sum()
+    }
+}
+fn process_square_case(input: u32, expected: u64) {
+    assert_eq!(grains::square(input), expected);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+
+    fn one() {
+        process_square_case(1, 1);
+    }
+
+    #[test]
+    fn two() {
+        process_square_case(2, 2);
+    }
+
+    #[test]
+    fn three() {
+        process_square_case(3, 4);
+    }
+
+    #[test]
+    fn four() {
+        process_square_case(4, 8);
+    }
+
+    #[test]
+    fn sixteen() {
+        process_square_case(16, 32_768);
+    }
+
+    #[test]
+    fn thirty_two() {
+        process_square_case(32, 2_147_483_648);
+    }
+
+    #[test]
+    fn sixty_four() {
+        process_square_case(64, 9_223_372_036_854_775_808);
+    }
+
+    #[test]
+    #[should_panic(expected = "Square must be between 1 and 64")]
+    fn square_0_raises_an_exception() {
+        grains::square(0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Square must be between 1 and 64")]
+    fn square_greater_than_64_raises_an_exception() {
+        grains::square(65);
+    }
+
+    #[test]
+    fn returns_the_total_number_of_grains_on_the_board() {
+        assert_eq!(grains::total(), 18_446_744_073_709_551_615);
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
